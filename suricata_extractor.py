61b3a820fd92a282e3065306fa1157cb26681f9b
eldraco <eldraco@gmail.com>
2017-06-01 14:44:28 +0200

    more

--- suricata_extractor.py
+++ suricata_extractor.py
@@ -16,6 +16,7 @@ import pandas as pd
 version = '0.1'
 
 timewindows = {}
+timeStampFormat = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 
@@ -40,30 +41,36 @@ class TimeWindow(object):
 def get_tw(col_time):
     """
     """
-    timeStampFormat = '%Y-%m-%dT%H:%M:%S.%f'
-    hourstring = col_time.split(':')[0]
     timestamp = datetime.strptime(col_time, timeStampFormat)
+    # Get the closest down time rounded
+    round_down_timestamp = roundTime(timestamp,timedelta(minutes=args.width), 'down')
+    str_round_down_timestamp = round_down_timestamp.strftime(timeStampFormat)
     try:
-        tw = timewindows[hourstring]
+        tw = timewindows[str_round_down_timestamp]
     except KeyError:
         # New tw
-        # Print old. Becareful with the order
-        try:
-            print '1'
-            output_tw(timewindows[timewindows.keys()[-1]])
-        except IndexError:
-            # No old tw
-            pass
-        tw = TimeWindow(hourstring)
+        # Get the previous TW id
+        prev_tw_date = round_down_timestamp - timedelta(minutes=int(args.width))
+        str_prev_round_down_timestamp = prev_tw_date.strftime(timeStampFormat)
+        output_tw(str_prev_round_down_timestamp)
+        tw = TimeWindow(str_round_down_timestamp)
         tw.set_start_time = timestamp
-        timewindows[hourstring] = tw
-        print 'New tw created at {}'.format(hourstring)
+        timewindows[str_round_down_timestamp] = tw
+        if args.verbose > 2:
+            print 'New tw created at {}'.format(str_round_down_timestamp)
     return tw
 
-def output_tw(tw):
+def output_tw(time_tw):
     """
+    Print the TW
     """
-    print tw
+    try:
+        tw = timewindows[time_tw]
+        if args.verbose > 1:
+            print 'Printing TW that started in: {}'.format(time_tw)
+        print tw
+    except KeyError:
+        return False
     for cat in tw.categories:
         print '\t{}: {}'.format(cat, tw.categories[cat])
     #plot(tw.categories)
@@ -99,19 +106,41 @@ def process_line(line):
     # Get the time window object
     current_tw = get_tw(col_time)
     current_tw.add_category(col_category)
+    return current_tw
+
+def roundTime(dt=None, date_delta=timedelta(minutes=1), to='average'):
+    """
+    Round a datetime object to a multiple of a timedelta
+    dt : datetime.datetime object, default now.
+    dateDelta : timedelta object, we round to a multiple of this, default 1 minute.
+    from:  http://stackoverflow.com/questions/3463930/how-to-round-the-minute-of-a-datetime-object-python
+    """
+    round_to = date_delta.total_seconds()
+    if dt is None:
+        dt = datetime.now()
+    seconds = (dt - dt.min).seconds
+    if to == 'up':
+        # // is a floor division, not a comment on following line (like in javascript):
+        rounding = (seconds + round_to) // round_to * round_to
+    elif to == 'down':
+        rounding = seconds // round_to * round_to
+    else:
+        rounding = (seconds + round_to / 2) // round_to * round_to
+    return dt + timedelta(0, rounding - seconds, -dt.microsecond)
 
 ####################
 # Main
 ####################
 if __name__ == '__main__':  
-    print 'Suricata Extractor. Version {}'.format(version)
-    print('https://stratosphereips.org')
+    print 'Suricata Extractor. Version {} (https://stratosphereips.org)'.format(version)
+    print
 
     # Parse the parameters
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', help='Amount of verbosity. This shows more info about the results.', action='store', required=False, type=int, default=1)
     parser.add_argument('-e', '--debug', help='Amount of debugging. This shows inner information about the flows.', action='store', required=False, type=int, default=0)
     parser.add_argument('-f', '--file', help='Suricata eve.json file.', action='store', required=False)
+    parser.add_argument('-w', '--width', help='Width of the time window to process. In minutes.', action='store', required=False, type=int, default=60)
     args = parser.parse_args()
 
     # Get the verbosity, if it was not specified as a parameter 
@@ -123,18 +152,20 @@ if __name__ == '__main__':
         args.debug = 0
 
     if args.file:
-        print 'Working with the file {} as parameter'.format(args.file)
+        if args.verbose > 1:
+            print 'Working with the file {} as parameter'.format(args.file)
         f = open(args.file)
         line = f.readline()
         while line:
-            process_line(line)
+            current_tw = process_line(line)
             line = f.readline()
-        print '2'
-        output_tw(timewindows[timewindows.keys()[-1]])
         f.close()
     else:
         for line in sys.stdin:
-            process_line(line)
-        print '3'
-        output_tw(timewindows[timewindows.keys()[-1]])
+            current_tw = process_line(line)
+    ## Print last tw
+    timestamp = datetime.strptime(current_tw.start_time, timeStampFormat)
+    round_down_timestamp = roundTime(timestamp,timedelta(minutes=args.width), 'down')
+    str_round_down_timestamp = round_down_timestamp.strftime(timeStampFormat)
+    output_tw(str_round_down_timestamp)
 
